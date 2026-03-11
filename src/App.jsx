6f4a0a14371dfa169dd5a5bdcee011ba328be38a
dev-vergent <vergelsantiago0509@gmail.com>
2026-03-11 22:01:26 +0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class Name="App">
+        <header className="App-header">
+          <h1>Welcome to my React App!</h1>
+          <p>
+            Name: Vergel Adrian Santiago<br/>
+            Email: vergelsantiago@example.com<br/>
+            Other Personal Info: Music enthusiast, 20 years old<br/>
+            <div>
+              <a href="https://github.com/vewnioh/Santiago-WebProg" target="github">Github Profile</a>
+            </div>
+          </p>
+          </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
