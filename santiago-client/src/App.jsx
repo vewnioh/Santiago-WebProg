4bf7d6ae727cf00afe298485f38343dfd9e798ff
dev-vergent <vergelsantiago0509@gmail.com>
2026-04-24 01:33:30 +0800

    lab-act1

---
+++ santiago-client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+      <div class Name="App">
+        <header className="App-header">
+          <h1>Welcome to my React App!</h1>
+          <p>
+            Name: Vergel Adrian Santiago<br/>
+            Email: vergelsantiago@example.com<br/>
+            Other Personal Info: Music enthusiast, 20 years old<br/>
+            <div>
+              <a href="https://github.com/vewnioh/Santiago-WebProg" target="github">Github Profile</a>
+            </div>
+          </p>
+          </header>
+      </div>
+  );
+}
+
+export default App
